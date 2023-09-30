c2a32d19404fc4a8e6b14fd9d8d3c03ddf0eef76
Kodandarao Chellapilli <briggs2019@go.byuh.edu>
2023-09-29 22:42:13 -1000

    Initial layout setup with CSS Grid

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,14 @@
 class Post < ApplicationRecord
-  validates :name, presence: true
-  validates :title, presence: true, length: { maximum: 255 }
-  validates :content, presence: true
-  validates :slug, uniqueness: true
-  validates :email, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
-  validates :status, inclusion: { in: %w(draft published) }
-  validates :views_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validate :custom_validation_method
-  validates :publish_date, presence: true, if: :published?
+  # app/models/post.rb
 
-  has_many :comments
-  validates_associated :comments
+
+  validates :picture_url, presence: true
+  validates :comment, presence: true
+  validates :likes, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  
+
+  
 
   private
 
